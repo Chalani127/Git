8ea14ce8cf295e3a4670e30fccc2a261534517a3
Chalani <chalanifdo721@gmail.com>
2025-03-22 17:03:02 +0530

    user input added

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 print("Basic Calculator:")
 
+
 def add(a, b):
     return a + b
 
@@ -13,3 +14,21 @@ def divide(a, b):
     if b == 0:
         return "Math Error"
     return a / b
+
+# Get user input
+input1 = float(input("Enter first number: "))
+input2 = float(input("Enter second number: "))6
+operator = input("Enter operator: ")
+
+if operator == '+':
+    result = add(input1, input2)
+elif operator == '-':
+    result = subtract(input1, input2)
+elif operator == '*':
+    result = multiply(input1, input2)
+elif operator == '/':
+    result = divide(input1, input2)
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
